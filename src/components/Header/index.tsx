25dd783eb20f5a43e87629b9fc7f946603ed5547
Mat Sz <contact@matsz.dev>
2020-08-27 18:23:23 +0200

    added a link back to the home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <section className={styles.header}>
-      <h1 className={styles.logo}>Serverboard</h1>
+      <Link to="/">
+        <h1 className={styles.logo}>Serverboard</h1>
+      </Link>
     </section>
   );
 };
