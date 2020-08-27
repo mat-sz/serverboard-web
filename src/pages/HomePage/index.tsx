e3da5774d6c0fe49db52b7311e0167479f69e129
Mat Sz <contact@matsz.dev>
2020-08-27 17:58:33 +0200

    added a basic home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import styles from './styles.module.scss';
+import ServerListItem from '../../components/ServerListItem';
+import { StateType } from '../../reducers';
 
 const HomePage: React.FC = () => {
-  return <></>;
+  const servers = useSelector((store: StateType) => store.servers);
+
+  return (
+    <ul className={styles.list}>
+      {servers.map(server => (
+        <ServerListItem server={server} key={server.id} />
+      ))}
+    </ul>
+  );
 };
 
 export default HomePage;
