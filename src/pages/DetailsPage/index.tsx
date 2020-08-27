aafc57982ad4ee84cc65ee6b0651b4651e002b65
Mat Sz <contact@matsz.dev>
2020-08-27 18:24:25 +0200

    moved content grid definitions to scss module

--- src/pages/DetailsPage/index.tsx
+++ src/pages/DetailsPage/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import styles from './styles.module.scss';
 import Sidebar from '../../components/Sidebar';
 import Details from '../../components/Details';
 
 const DetailsPage: React.FC = () => {
   return (
-    <div className="content">
+    <div className={styles.content}>
       <Sidebar />
       <Details />
     </div>
